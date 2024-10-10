8767735875c82513ee86a8c4a1b7b68c92b01193
macdaegeon <chleorjs37@gmail.com>
2024-10-10 21:16:41 +0900

    eslint 수정 진행중

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import App from "./App";
-import React from "react";
+import App from './App'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+const a = 1
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
