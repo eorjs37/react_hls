d24f68b56c86b4268f87d538a5ca073e828cf864
macdaegeon <chleorjs37@gmail.com>
2024-10-19 09:53:05 +0900

    context api 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
+import { createContext } from 'react'
 import About from './pages/about/About'
 import Home from './pages/Home'
+const context = createContext('right')
 const App = () => {
   return (
-    <Routes>
-      <Route path={'/'} element={<Home />}></Route>
-      <Route path={'/about'} element={<About />}></Route>
-    </Routes>
+    <context.Provider value={'right'}>
+      <Routes>
+        <Route path={'/'} element={<Home />}></Route>
+        <Route path={'/about'} element={<About />}></Route>
+      </Routes>
+    </context.Provider>
   )
 }
 
