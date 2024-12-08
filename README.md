24837e36763b565fcea8e24667c4d16861177159
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-12-09 08:42:59 +0900

    UseEffect 사용

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## useCallback
 
 ## useEffect
+
+```
+state의 변화를 감지하여 동작하는 함수
+```
--- src/pages/dummy/Dummy.tsx
+++ src/pages/dummy/Dummy.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function Dummy() {
   const [isOnline, setIsOnline] = useState(true)
-
+  const [clickCount, setClickCount] = useState(0)
   const onClickOnline = () => {
     setIsOnline(!isOnline)
   }
+  const onClickCount = () => {
+    setClickCount(clickCount + 1)
+  }
+
+  useEffect(() => {
+    console.log(clickCount)
+  }, [clickCount])
+
   return (
     <div>
       <h1>{isOnline ? '✅ 온라인' : '❌ 연결 안 됨'}</h1>
+      <h1>{clickCount}</h1>
       <button onClick={onClickOnline}>{'연결버튼'}</button>
+      <button onClick={onClickCount}>{'카운트 버튼'}</button>
     </div>
   )
 }
